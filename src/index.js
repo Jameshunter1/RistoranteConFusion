c81e177f98fb3e7c397be7700d0d3de35cc81ba1
James Hunter <jameshunter1017@live.com>
2023-04-07 17:46:32 -0400

    Redux Actions

--- src/index.js
+++ src/index.js
@@ -1,23 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-import { Provider } from 'react-redux';
 import App from './App';
-import { configureStore } from '@reduxjs/toolkit';
-import rootReducer from './redux/reducer';
 import './index.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-
-
-const store = configureStore({
-  reducer: rootReducer
-});
-
 render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>,
-  document.getElementById('root')
+ <React.StrictMode>
+  <App />
+ </React.StrictMode>,
+ document.getElementById('root'),
 );
--- src/redux/configureStore.js
+++ src/redux/configureStore.js
@@ -1,8 +1,10 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { createForms } from 'react-redux-form';
 import { Dishes } from './dishes';
 import { Comments } from './comments';
 import { Promotions } from './promotions';
 import { Leaders } from './leaders';
+import thunk from 'redux-thunk';
 
 export const ConfigureStore = () => {
  const store = createStore(
